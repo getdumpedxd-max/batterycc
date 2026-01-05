5446aa6a057485aa612f46c647e1c30559c05446
getdumpedxd-max <getdumpedxd@gmail.com>
2026-01-05 12:45:28 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,6 +4,10 @@ local repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
+ThemeManager:SetLibrary(Library)
+ThemeManager:SetTheme({
+    Accent = Color3.fromRGB(160, 90, 255)
+})
 local Window = Library:CreateWindow({ Title = '                     $ Battery.cc | Beta $                     ', AutoShow = true, TabPadding = 15, MenuFadeTime = 0.2 })
 local Tabs = { Main = Window:AddTab('Main'), Character = Window:AddTab('Character'), Visuals = Window:AddTab('Visuals'), Misc = Window:AddTab('Misc'), Players = Window:AddTab('Players'), ['UI Settings'] = Window:AddTab('UI Settings') }
 local GunMods = Tabs.Main:AddRightGroupbox('Gun Mods')
