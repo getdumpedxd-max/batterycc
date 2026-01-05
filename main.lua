63464cf6119b042dbf5c2bb7ec5de16918b1679e
getdumpedxd-max <getdumpedxd@gmail.com>
2026-01-04 18:25:09 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,5 @@
 -- https://discord.gg/UgQAPcBtpy
 
-lohttps://github.com/getdumpedxd-max/batterycc/tree/maincal repo = 'https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/'
 local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
 local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
