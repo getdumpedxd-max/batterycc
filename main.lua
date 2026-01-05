1f5f834708e47df250b994ff51e4601f097911f4
getdumpedxd-max <getdumpedxd@gmail.com>
2026-01-05 12:47:37 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,9 +8,7 @@ ThemeManager:SetLibrary(Library)
 ThemeManager:SetTheme({
     Accent = Color3.fromRGB(160, 90, 255)
 })
-local Window = Library:CreateWindow({ Title = '                     $ Battery.cc | Beta $                     ', AutoShow = true, TabPadding = 15, MenuFadeTime = 0.2 })
-local Tabs = { Main = Window:AddTab('Main'), Character = Window:AddTab('Character'), Visuals = Window:AddTab('Visuals'), Misc = Window:AddTab('Misc'), Players = Window:AddTab('Players'), ['UI Settings'] = Window:AddTab('UI Settings') }
-local GunMods = Tabs.Main:AddRightGroupbox('Gun Mods')
+local Window = Library:CreateWindow({ Title = "$ Battery.cc | Beta $", AutoShow = true, TabPadding = 15, MenuFadeTime = 0.2 })
 local KillAura = Tabs.Main:AddRightGroupbox('Combat')
 
 --game.Players.LocalPlayer.Character.Humanoid.Health = 0
